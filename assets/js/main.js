b388ca62d9d229830366d530d843e276ac90f9f3
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-07 15:35:05 +0100

    change let with const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 
 // per ogni passaggio JS faccio un console.log
 
-// 1. Chiedo all'utente il suo nome con un prompt e lo salvo in un let firstName
+// 1. Chiedo all'utente il suo nome con un prompt e lo salvo in un const firstName
 
-let firstName = prompt('Inserisci il tuo nome');
+const firstName = prompt('Inserisci il tuo nome');
 
 console.log(firstName);
 
 
-// 2. Chiedo all'utente il suo cognome con un prompt e lo salvo in un let lastName
+// 2. Chiedo all'utente il suo cognome con un prompt e lo salvo in un const lastName
 
-let lastName = prompt('Inserisci il tuo cognome');
+const lastName = prompt('Inserisci il tuo cognome');
 
 console.log(lastName);
 
-// 3. Chiedo all'utente il suo colore preferito con un prompt e la salvo in un let color
+// 3. Chiedo all'utente il suo colore preferito con un prompt e la salvo in un const color
 
-let color = prompt('Inserisci il tuo colore');
+const color = prompt('Inserisci il tuo colore');
 
 console.log(color);
 
@@ -24,7 +24,7 @@ console.log(color);
 
 // 5. seleziono l'h1 con getElementById e lo salvo in una variabile
 
-let password = document.getElementById( 'password' )
+const password = document.getElementById( 'password' )
 
 console.log(password);
 
