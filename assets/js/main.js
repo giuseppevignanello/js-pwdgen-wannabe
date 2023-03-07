c70b2c8960b1182a371ae590a84f08310dc389d2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-07 14:15:42 +0100

    problem analysis

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+// per ogni passaggio JS faccio un console.log
+
+// 1. Chiedo all'utente il suo nome con un prompt e lo salvo in un let firstName
+// 2. Chiedo all'utente il suo cognome con un prompt e lo salvo in un let lastName
+// 3. Chiedo all'utente la sua età  con un prompt e la salvo in un let age
+
+// 4. creo un h1 nell'html 
+
+// 5. selziono l'h1 con getElementById e lo salvo in una variabile
+
+// 6. assegno all'innerHTML un nuovo valore di "23" interpolando prima le tre variabili firstName lastName e age
+
+
+
+
+
